d1befa9431f3d546381836570b0b57507e732e59
zar <64859915+zar-hub@users.noreply.github.com>
2022-01-10 19:40:13 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,2 +1,4 @@
-sudo snap install alacritty todoist teams
+sudo apt-get install snapd flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo snap install alacritty todoist teams 
 sudo flatpak install io.github.shiftey.Desktop
