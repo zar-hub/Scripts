b4ded0610ea2e12f4f661c06500ee929d51ef8c0
zar <64859915+zar-hub@users.noreply.github.com>
2022-01-10 17:55:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 # SECTION ~ Hi
 echo "Hi, startup script initialized..."
+
 # SECTION ~ sudoers privileges
 echo "Adding your user to sudoers... Currently the only user that will be added is 'zar'"
+su -
+echo -e "# DEBIAN_LEMON_FLAVOR: grant permissions\nzar ALL=(ALL) NOPASSWD:ALL" >> file.txt
+
 # SECTION ~ Syncthing https://apt.syncthing.net/ 
 # Add the release PGP keys:
 sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
